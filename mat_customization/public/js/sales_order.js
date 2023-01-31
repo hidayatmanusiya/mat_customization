2f01eed225fb8428e284d7d20c3dc8308eaf5ebf
Jaymin28 <jaymintarpara28@gmail.com>
2023-01-31 11:59:54 +0530

    Frontend Working Hours

--- mat_customization/public/js/sales_order.js
+++ mat_customization/public/js/sales_order.js
@@ -15,17 +15,18 @@ frappe.ui.form.on('Sales Order Item', {
 
 var calculate_working_hour = function (frm, cdt, cdn) {
 	var row = locals[cdt][cdn];
-	frappe.call({
-		method: 'mat_customization.utils.calculate_working_hour',
-		type: 'POST',
-		args: {
-			"item": row
-		},
-		callback: function (r) {
-			if (!r.exc) {
-				frappe.model.set_value(cdt, cdn, "working_hours", r.message)
+	if (row.start_date && row.end_date && row.holiday_list && row.uom)
+		frappe.call({
+			method: 'mat_customization.utils.calculate_working_hour',
+			type: 'POST',
+			args: {
+				"item": row
+			},
+			callback: function (r) {
+				if (!r.exc) {
+					frappe.model.set_value(cdt, cdn, "working_hours", r.message)
+				}
 			}
-		}
-	});
+		});
 
 }
